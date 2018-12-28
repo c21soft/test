560e639c49e95076ff8b93154ff3cbc43246b533
ryu <ryu@ryu-PC>
2018-12-28 23:07:57 +0900

    처음 커밋합니다.

---
+++ test1/src/test1/HellowWorld.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("æ»≥Á«œººø‰. Hello Word!");
+	}
+
+}
