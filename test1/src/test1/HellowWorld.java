b9a1480473277510b12814c815f9ba039433097a
ryu <ryu@ryu-PC>
2018-12-29 21:09:15 +0900

    수정1

--- test1/src/test1/HellowWorld.java
+++ test1/src/test1/HellowWorld.java
@@ -5,6 +5,7 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요. Hello Word!");
+		System.out.println("안녕하세요. 수정합니다.");
 	}
 
 }
